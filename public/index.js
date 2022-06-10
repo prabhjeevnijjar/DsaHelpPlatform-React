678b952767fa6f582eb738e312fe5709b931408b
Prabhjeev Nijjar <prabhjeev@so.city>
2022-06-10 15:29:11 +0530

    initial setup

---
+++ public/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Login } from "../src/pages/login/Login";
+const app = document.getElementById("root");
+ReactDOM.render(<Login />, app);
